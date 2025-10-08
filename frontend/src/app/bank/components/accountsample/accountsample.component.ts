40a871850665ff640879d42de49709e80300d66c
username <wecp-dummy@gmail.com>
2025-10-08 14:44:56 +0000

    wecp initial commit

--- frontend/src/app/bank/components/accountsample/accountsample.component.ts
+++ frontend/src/app/bank/components/accountsample/accountsample.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AccountTS } from '../../types/tstypes/Accountts';
+import { Account } from '../../types/Account';
 
 @Component({
   selector: 'app-accountsample',
@@ -9,4 +11,10 @@ import { Component } from '@angular/core';
 })
 export class AccountsampleComponent {
 
+  account : AccountTS ;
+  formattedBalance :string;
+  constructor(){
+    this.account = new AccountTS("1",1000,"1")
+    this.formattedBalance=this.account.balance.toFixed(2);
+  }
 }
